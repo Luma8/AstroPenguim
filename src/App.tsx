ce3d3fc8bcb12127899375a3f8c4ca49e08ec835
luma8 <lucasrobbot@gmail.com>
2022-07-09 01:31:04 -0400

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home/Home"
+import { GlobalStyle } from "./styles/GlobalStyles"
+
+export default function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <Home />
+    </>
+  )
+}
+
